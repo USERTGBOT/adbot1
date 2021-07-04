47b50be46f177628f6b4ebfc2810663c22385cfd
USERTGBOT <86607541+USERTGBOT@users.noreply.github.com>
2021-07-04 13:16:36 +0530

    ♥️

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+define('ADMIN_NAME', 'ILEEADS ADMIN');
+define('ADMIN_USERNAME', 'ILEE_ADMIN');
+define('ADMIN_CHAT_ID', 1094274287);
+define('AD_SERVICE_NAME', '𝐈𝐋𝐄𝐄 𝐀𝐃𝐒');
+define('CHANNEL_USERNAME', 'ILEE_ADS');
+define('PAYOUT_GROUP_ID', -10012844958);
+define('REGISTER_BOT_USERNAME', 'ILEEads_bot');
+define('POST_BOT_USERNAME', '@ILEEads_bot');
+define('BOT_ID', '934988447');
+
+$dbusr = 'user';
+$dbpas = 'pass';
+$dbnam = 'dbname';
+
+$token = 'TOKEN HERE';
+
+$botcreator = '1094274287'; //BOT Maker
+$paylink = "null"; 
+$fname = "ilee"; //folder name
+
